8d8ce190c8cbe5e8b2df00db17fe23a8d72deceb
jesus <jesuspereira@correo.ugr.es>
2025-12-06 13:04:22 +0100

    Working version P3

---
+++ PR3_DBA/src/Agente/comportamientos/EstadosAgente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Agente.comportamientos;
+
+/**
+ *
+ * @author jesusp
+ */
+public enum EstadosAgente {
+    INICIO_MISION_SALVAR_NAVIDAD, ESPERANDO_TRADUCCION_ELFO_1, ESPERANDO_TRADUCCION_ELFO_2, ESPERANDO_CONFIRMACION_SANTA, ESPERANDO_COORDENADAS_RENO_PERDIDO, MOVIENDOSE, SOLICITAR_NUEVA_COORDENADA_RENO
+}
